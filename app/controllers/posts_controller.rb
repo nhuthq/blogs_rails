3e5deccc3132967f2767d0c8d4c0a85460e79516
nhuthq <>
2024-01-10 12:27:39 +0700

    add bootstrap

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,79 @@
+class PostsController < ApplicationController
+
+  before_action :authenticate_author!, except: [:index, :show]
+  before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :correct_author, only: [:edit, :update, :destroy]
+
+  # GET /posts or /posts.json
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/1 or /posts/1.json
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = current_author.posts.build
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @post = current_author.posts.build(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1 or /posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1 or /posts/1.json
+  def destroy
+    @post.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Check correct account
+    def correct_author
+      @friend = current_author.posts.find_by(id: params[:id])
+      redirect_to posts_path, notice: 'Not Authorized To Edit This Post' if @post.nil?
+    end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:title, :description, :published, :published_at, :author_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :authors
+  resources :posts
+  get 'home/index'
 
-  root to: "home#index"
+  root 'posts#index'
 end
